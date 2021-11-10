608dd24ae567c3c7d5db2818020590479574a794
ailisipai <13585951973@163.com>
2021-11-11 00:08:37 +0800

    功能验证

--- vue.config.js
+++ vue.config.js
@@ -101,11 +101,11 @@ const vueConfig = {
 
   devServer: {
     // development server port 8000
-    port: 8000,
+    port: 8001,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
      proxy: {
        '/api': {
-         target: 'http://localhost:8081',
+         target: 'http://localhost:8082',
          ws: false,
          changeOrigin: true,
          pathRewrite: {
